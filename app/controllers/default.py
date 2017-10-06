3b7b7de58c329042286b13fdeb88f9fd1ec71285
vitorrios1001 <vitorluizrios@hotmail.com>
2017-10-06 19:36:17 -0300

    Project twitter in flask

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,21 @@
 from app import app
+from flask import render_template
+
+from app.models.forms import LoginForm
+
+@app.route("/index/<user>")
+@app.route("/", defaults={"user":None})
+def index(user):
+    return render_template('index.html', user=user)
 
 
-@app.route("/")
-def index():
-    return "Hello world"
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    else:
+        print(form.errors)
+
+    return render_template('login.html', form=form)
